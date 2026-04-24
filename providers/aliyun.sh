71ae7ec84e2a512847af447e5e132653ee192775
liaofp <liaofp@gmail.com>
2026-04-24 12:58:55 +0800

    modify

--- providers/aliyun.sh
+++ providers/aliyun.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 python3 -c "
-from aligo import Aligo
-ali = Aligo(refresh_token='$2')
-folder = ali.get_folder_by_path('YT_Temp', create_when_empty=True)
-res = ali.upload_file('$1', parent_file_id=folder.file_id)
+from .aliclient import AliClient
+ali = AliClient(refresh_token='$2')
+res = ali.upload_file('$1')
 print(f'---RESULT_START---\nDRIVE_ID: {res.drive_id}\nFILE_ID: {res.file_id}\nFILE_NAME: {res.name}\n---RESULT_END---')
 "
